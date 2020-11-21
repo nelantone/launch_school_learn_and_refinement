4ef5226d1d4df5186818a39688b5c9bde895c7c0
Tonio Serna <antonelo@gmail.com>
2020-11-21 23:32:41 +0100

    Rubocop update  & pseudocode excercise

---
+++ 101-109/lesson_2_small_programs/pseudocode.rb
@@ -0,0 +1,76 @@
+=begin
+1. a method that returns the sum of two integers
+## casual pseudocode
+we initialize a method who has 2 integer arguments
+we initialize the variable result and inside we add the first argument
+adding the sum operator continued by the second argument
+we print the result variable
+we close the method
+invoke the method passing two integers
+we have the result
+
+## formal pseudocode
+START
+SET sum_of_integers method
+SET first argument
+SET second argument
+inside the variable
+SET result as the first integer plus the second integer
+PRINT result
+END
+GET sum_of_integers with two integers as arguments
+PRINT result as last ruby linde inside the method
+END of program
+
+2. a method that takes an array of strings, and returns a string that is all
+those strings concatenated together
+##casual pseudocode
+
+we initialize a method called concatenator
+we set an argument as an array of strings
+we use a method to take each string and to concatenate saving the concatenation
+to one variable
+we call puts to print the variable as last line of the code inside the method.
+we return the value
+we close the method
+we invoke the method with a varieble of strings as argument
+
+##formal pseudocode
+START
+SET method concatenator
+SET argument as array of strings
+WHILE passing from the array each string
+GET the output from the iteration to a new variable result
+PRINT result variable
+END
+GET concatenator with an array of strings an argument
+PRINT result
+END of program
+
+3. a method that takes an array of integers, and returns a new array with
+every other element
+##casual pseudocode
+We initialize a method new_random_ary_creator with an array of integers as
+argument
+inside the method we initialize a new array
+we iterate from the original array generating random elements that are different
+from this array cheking there is a new element adding eache element to the new
+array.
+we print the new array
+we end the method.
+We call the method
+we have a printed result
+##formal pseudocode
+START program
+SET method new_random_ary_creator
+SET argument  that will be an array of integers
+SET a new array
+WHILE each item of array of integers we creat a random item who is different to
+all of the itemes contained inside this array
+GET the item and add each item inside of the new array
+GET the new array as the result
+END program
+GET method new_random_ary_creator with array of integers
+GET the new array as result
+ENF of program
+end=
--- Gemfile
+++ Gemfile
@@ -5,4 +5,4 @@ source 'https://rubygems.org'
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 gem 'guard', '~> 2.14'
-gem 'rubocop', '~> 0.42.0'
+gem 'rubocop', '~> 0.86.0'
