4c30585d3a784a6187e6d121230d2aa6deeff4e9
Tonio Serna <antonelo@gmail.com>
2020-11-22 23:36:56 +0100

    refactor calculator

--- 101-109/lesson_2_small_programs/calculator.rb
+++ 101-109/lesson_2_small_programs/calculator.rb
@@ -15,27 +15,71 @@
 # ask the user to shoose between add, substract, miltilply or divide. input 2
 # perform the operation
 # display the result to the user. final output
+first_operator = 0
+second_operator = 0
 
-# Kernel.puts('Please add the first operator')
-# first_operator = Kernel.gets.chomp.to_i
-# Kernel.puts('Add the second operator')
-# second_operator = Kernel.gets.chomp.to_i
-# Kernel.puts("Add the operation you want to do '+','-','*','/'")
-# operation = Kernel.gets.chomp
+def prompt(text)
+  puts ">> #{text}"
+end
 
 def operation(first_operator, second_operator, operation)
   case operation
   when '+'
-    Kernel.puts(first_operator + second_operator)
+    prompt('Addition')
+    prompt(first_operator + second_operator)
   when '-'
-    Kernel.puts(first_operator - second_operator)
+    prompt('Substraction')
+    prompt(first_operator - second_operator)
   when '*'
-    Kernel.puts(first_operator * second_operator)
+    prompt('Multiplication')
+    prompt(first_operator * second_operator)
   when '/'
-    Kernel.puts(first_operator / second_operator.to_f)
-  else
-    Kernel.puts('This operation does not extist in this calculator')
+    prompt('Division')
+    prompt(first_operator / second_operator.to_f)
   end
 end
 
-operation(first_operator, second_operator, operation)
+def operation_selector
+  prompt("Add the operation you want to do:
+  type '+' to addition,
+  type '-' to substraction,
+  type '*' to multiplication,
+  type '/' to division ")
+  Kernel.gets.chomp
+end
+
+def operation_validator(operation)
+  loop do
+    break if %w(+ - * /).include? operation
+    prompt("Please write a valid operation ")
+    operation = operation_selector
+  end
+  operation
+end
+
+loop do # main_loop
+  loop do
+    prompt('Please add the first operator')
+    first_operator = Kernel.gets.chomp.to_i
+    break if first_operator != 0
+    prompt('Type a correct number(integer or number different than 0)')
+  end
+
+  loop do
+    prompt('Add the second operator')
+    second_operator = Kernel.gets.chomp.to_i
+    break if second_operator != 0
+    prompt('Type a correct number(integer or number different than 0)')
+  end
+
+  operation = operation_validator(operation_selector)
+
+  operation(first_operator, second_operator, operation)
+
+  prompt("Do you want to do another operation? Type 'Y'")
+  question = Kernel.gets().chomp()
+
+  break unless question.downcase == 'y'
+end
+
+prompt('Thanks for using the calculator')
