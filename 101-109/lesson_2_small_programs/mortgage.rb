1960bc6493d6ddecc07f4227262b674e8c5be534
Tonio Serna <antonelo@gmail.com>
2020-11-24 00:38:43 +0100

    refactoring and rubocop from mortgage calculator

--- 101-109/lesson_2_small_programs/mortgage.rb
+++ 101-109/lesson_2_small_programs/mortgage.rb
@@ -17,7 +17,6 @@ p = loan amount
 j = monthly interest rate
 n = loan duration in months
 
-
 month_paym = loan_amount * (month_int_r / (1 - (1 + month_int_r)**(-loan_dur)))
 
 I want to know how much I will pay every month for my loan.
@@ -31,7 +30,9 @@ def prompt(text)
 end
 
 Kernel.puts("== Welcome to the mortgage calculator == \n\n".upcase)
-prompt("please add the loan ammount")
+prompt("Please Add your currency name")
+currency= Kernel.gets().chomp()
+prompt("Add the loan ammount")
 loan_amount = Kernel.gets().chomp().to_f
 prompt("Add the annual percentage rate")
 annual_percent_rate = Kernel.gets().chomp().to_f
@@ -39,37 +40,36 @@ prompt("Add your loan duration in months")
 loan_duration_months = Kernel.gets().chomp().to_i
 prompt("This is your information:")
 prompt("\n================================\n
-Your loan ammount: #{loan_amount}\n
+Your loan ammount: #{loan_amount} #{currency}\n
 your percent rate is #{annual_percent_rate}% \n
-yout loan duration #{loan_duration_months} in months \n
-loan duration in years #{loan_duration_months/12} \n
+your loan duration #{loan_duration_months} in months \n
+loan duration in years #{loan_duration_months / 12} \n
 ================================")
 
 # loan_amount
 
-annual_rate = annual_percent_rate/100
+annual_rate = annual_percent_rate / 100
 
-month_int_rate = annual_rate/12
+month_int_rate = annual_rate / 12
 
-def total_month_interest(month_int_rate,loan_duration_months)
+def month_int_ratio_share(month_int_rate, loan_duration_months)
   (month_int_rate / (1 - (1 + month_int_rate)**(-loan_duration_months)))
 end
 
 def month_paym(loan_amount, month_int_rate, loan_duration_months)
-  loan_amount * total_month_interest(month_int_rate,loan_duration_months)
+  loan_amount * month_int_ratio_share(month_int_rate, loan_duration_months)
 end
 
 monthly_payment = month_paym(loan_amount, month_int_rate, loan_duration_months)
-total_month_interest_amount = total_month_interest(month_int_rate,loan_duration_months)
 
 prompt("These are your results:")
 prompt("\n================================\n
 Monthly payment:
-#{monthly_payment.round(2)} \n
+#{monthly_payment.round(2)} #{currency}\n
 Total debt:
-#{(monthly_payment * loan_duration_months).round(2)}\n
+#{(monthly_payment * loan_duration_months).round(2)} #{currency}\n
 Only interests debt:
-#{((monthly_payment * loan_duration_months).round(2) - loan_amount).round(2)}\n
+#{((monthly_payment * loan_duration_months) - loan_amount).round(2)} #{currency}
+
 
 ================================")
-
