48d1fd17150517605e4c4b1858a54b044506dddd
Tonio Serna <antonelo@gmail.com>
2020-12-05 23:12:35 +0100

    Add some small exercises

---
+++ 101-109/lesson_2_small_programs/greeting_scream_usr.rb
@@ -0,0 +1,11 @@
+
+
+loop do
+  puts "What is your name?"
+  name = gets.chomp
+
+  puts "\n Hello #{name} \n"
+  puts ("\n Hello #{name.chop}. Why are we screaming? \n").upcase if (name.chars.last == '!')
+
+  puts "\n 'ctl-c to exit' \n \n"
+end
