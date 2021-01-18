1e9cf64c4879bc7e57ede7a1ecbe6522b570db68
Tonio Serna <antonelo@gmail.com>
2021-01-19 00:32:30 +0100

    Add 6.2 Delete vowels from small problems +

---
+++ 101-109/exercises-small_problems/6_2_delete_vowels.rb
@@ -0,0 +1,16 @@
+#Delete vowels
+# Write a method that takes an array of strings, and returns an array of the
+# same string values, except with the vowels (a, e, i, o, u) removed.
+
+#Example:
+remove_vowels(%w(abcdefghijklmnopqrstuvwxyz)) == %w(bcdfghjklmnpqrstvwxyz)
+remove_vowels(%w(green YELLOW black white)) == %w(grn YLLW blck wht)
+remove_vowels(%w(ABC AEIOU XYZ)) == ['BC', '', 'XYZ']
+
+def remove_vowels(array)
+  array.map { |str| str.split(/[⌃aeiou]/i).join } #last `i` is case insensitive
+end
+
+def remove_vowels(array)
+  strings.map { |string| string.delete('aeiouAEIOU') }
+end
