72d9a86f5939751fb3f916955df380cbde1d2ca7
Tonio Serna <antonelo@gmail.com>
2021-02-13 20:21:43 +0100

    Add 10.8 'fibonacci number recursion' small problems

---
+++ 101-109/exercises-small_problems/10_8_fibonacci_numbers_recursion.rb
@@ -0,0 +1,88 @@
+
+=begin
+Fibonacci Numbers (Recursion)
+The Fibonacci series is a sequence of numbers starting with 1 and 1 where
+each number is the sum of the two previous numbers: the 3rd Fibonacci number
+is 1 + 1 = 2, the 4th is 1 + 2 = 3, the 5th is 2 + 3 = 5, and so on.
+In mathematical terms:
+
+F(1) = 1
+F(2) = 1
+F(n) = F(n - 1) + F(n - 2) where n > 2
+Sequences like this translate naturally as "recursive" methods.
+A recursive method is one in which the method calls itself. For example:
+
+def sum(n)
+  return 1 if n == 1
+  n + sum(n - 1)
+end
+
+sum is a recursive method that computes the sum of all integers
+between 1 and n.
+
+Recursive methods have three primary qualities:
+
+They call themselves at least once.
+They have a condition that stops the recursion (n == 1 above).
+They use the result returned by themselves.
+In the code above, sum calls itself once; it uses a condition of n == 1
+to stop recursing; and, n + sum(n - 1) uses the return value of the
+recursive call to compute a new return value.
+
+Write a recursive method that computes the nth Fibonacci number,
+where nth is an argument to the method.
+
+If you find yourself struggling to understand recursion, see this
+forum post. It's worth the effort learning to use recursion.
+
+That said, this exercise is a lead-in for the next two exercises.
+It verges on the Advanced level, so don't worry or get discouraged if
+you can't do it on your own. Recursion is tricky, and even experienced
+developers can have trouble dealing with it.
+=end
+=begin
+- Problem understanding:
+  - problem domain:
+    - Fibonacci numbers: sequence of numbers starting with 1 and 1 where
+        each number is the sum of the two previous numbers
+    - Recursion: A recursive method is one in which the method calls itself.
+        Recursive methods have three primary qualities:
+      - They call themselves at least once.
+      - They have a condition that stops the recursion (n == 1 above).
+      - They use the result returned by themselves.
+  - mental model: Write a recursive method that computes the nth Fibonacci
+    number, where nth is an argument to the method.
+  - input: number
+  - output: number
+  - rules:
+    - explicit:
+      - we need to use recursive method(and )
+      - sequence of numbers starting with 1
+    - implicit:
+- Examples(below):
+- Data Structure:
+  Maybe an array, not sure if it's a need.
+- Algorithm:
+  start adding the most basic examples and the easy result without recursion
+  - add the first number and save the result as new value for the next iteration
+    let's start from there...
+- Code:
+=end
+
+def fibonacci(numth)
+  return 1 if numth == 1 || numth == 2
+
+  fibonacci(numth - 2) + fibonacci(numth - 1)
+end
+
+# Examples:
+
+fibonacci(1) == 1 # starting with one
+fibonacci(2) == 1 # 0 + (1) = (1)
+fibonacci(3) == 2 # 1 + (1) = (2)
+fibonacci(4) == 3 # (1) + (2) = (3) The key! As fibonacci(1) + fibonacci(2) = 3
+fibonacci(5) == 5 #  (2) + (3) = 5
+fibonacci(6) == 8 #  (3) + (5) = (8)
+fibonacci(7) == 8 #  (5) + (8) = 13
+fibonacci(12) == 144
+fibonacci(20) == 6765
