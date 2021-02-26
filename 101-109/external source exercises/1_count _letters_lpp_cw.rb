074f523beb95fae32fc538b9e499a13a7f83ff96
Tonio Serna <antonelo@gmail.com>
2021-02-26 18:17:09 +0100

    LPP-1 'Count letters' CW/109

---
+++ 101-109/external source exercises/1_count _letters_lpp_cw.rb
@@ -0,0 +1,22 @@
+# 1.Count letters in string
+# (https://www.codewars.com/kata/5808ff71c7cfa1c6aa00006d/train/ruby)
+# 6 kyu
+
+=begin
+Count letters in string
+In this kata, you've to count lowercase letters in a given string and return the
+letter count in a hash with 'letter' as key and count as 'value'. The key must
+be 'symbol' instead of string in Ruby and 'char' instead of string in Crystal.
+
+Example:
+
+letterCount('arithmetics') #=> {:a=>1, :c=>1, :e=>1, :h=>1, :i=>2, :m=>1, :r=>1, :s=>1, :t=>2}
+=end
+
+def letter_count(string)
+  string.chars.tally.transform_keys(&:to_sym)
+end
+
+p letter_count('codewars') == {:a=>1, :c=>1, :d=>1, :e=>1, :o=>1, :r=>1, :s=>1, :w=>1}
+p letter_count('activity') == {:a=>1, :c=>1, :i=>2, :t=>2, :v=>1, :y=>1}
+p letter_count('arithmetics') == {:a=>1, :c=>1, :e=>1, :h=>1, :i=>2, :m=>1, :r=>1, :s=>1, :t=>2}
