c5c0a6cff34b0f35a875c6a46756c020d1757fea
Tonio Serna <antonelo@gmail.com>
2021-03-04 19:05:22 +0100

    Add L6 Tic-tac-toe:  winner, tie, play-again, WIP: some little issues on results

--- 101-109/lesson_6_slightly_larger_programs/tic_tac_toe.rb
+++ 101-109/lesson_6_slightly_larger_programs/tic_tac_toe.rb
@@ -1,3 +1,4 @@
+require 'pry-byebug'
 =begin
 tictactoe for one player
 -Problem understanding
@@ -35,26 +36,29 @@ tictactoe for one player
 - Code
 =end
 
+WIN_RESULTS = [ %w(00 01 02), %w(10 11 12), %w(20 21 22), %w(00 11 22),
+                  %w(02 11 20), %w(00 10 20), %w(01 11 21), %w(02 12 22) ]
+
 def display_matrix(m)
   puts ''
   puts "#{m[0][0]} | #{m[0][1]} | #{m[0][2]}".center(28)
-  puts ' '*7 + '- '*7 + ' '*7
+  puts ' ' * 7 + '- ' * 7 + ' ' * 7
   puts "#{m[1][0]} | #{m[1][1]} | #{m[1][2]}".center(28)
-  puts ' '*7 + '- '*7 + ' '*7
+  puts ' ' * 7 + '- ' * 7 + ' ' * 7
   puts "#{m[2][0]} | #{m[2][1]} | #{m[2][2]}".center(28)
   puts ''
 end
 
 def free_spots(matrix)
   matrix.map.with_index do |row_spot, idx_r|
-    row_spot.map.with_index do |col_spot, idx_c|
-      [idx_r,idx_c] if matrix[idx_r][idx_c] == ' '
+    row_spot.map.with_index do |_, idx_c|
+      [idx_r, idx_c] if matrix[idx_r][idx_c] == ' '
     end
-  end.map {|row| row.delete_if(&:nil?)}
+  end.map { |row| row.delete_if(&:nil?) }
 end
 
 def display_spots(matrix)
-  free_spots(matrix).map {|row| p row.map {|item| item.join }}
+  free_spots(matrix).map { |row| p row.map(&:join) }
 end
 
 def play(player_row_col, symbol, m)
@@ -64,45 +68,140 @@ def play(player_row_col, symbol, m)
   m[row_player][col_player] = symbol
 end
 
-puts 'Welcome to Tic Tac Toe!'.center(28)
-
-m = [
-      [' ',' ',' '],
-      [' ',' ',' '],
-      [' ',' ',' ']
-    ]
-
-loop do
-  single_ary_free_spots = free_spots(m).flatten(1).map(&:join)
-
-  display_matrix(m)
-
-  puts 'Please mark a square on the game'
-  puts "your free spots are:"
-  puts ''
-
-  display_spots(m)
-
-  puts ''
-  puts 'For example top left is 00, bottom right 22, center 11'
-  puts 'choose a row and column/item'
-  row_col_usr = gets.chomp
-
-  single_ary_free_spots.include?(row_col_usr)
-
-  until single_ary_free_spots.include?(row_col_usr)
-    puts ''
-    puts 'Error:'
-    puts "#{row_col_usr} not valid, please check again the valid options:"
-    puts ''
-    display_spots(m)
-    puts 'choose a row and column/item'
-    row_col_usr = gets.chomp
-  end
-
-  play(row_col_usr, 'X', m)
-
-  row_col_computer = single_ary_free_spots.sample
-  play(row_col_computer, 'O', m)
+def player_won?(matrix)
+  WIN_RESULTS.map {|win_res| win_res.all? { |el| matrix.include? el } }.any?
 end
 
+def show_player_results(matrix, symbol)
+  total = []
+  matrix.each_with_index do |row, idx_r|
+    row.each_with_index do |elem, idx_c|
+      total << "#{idx_r}#{idx_c}" if elem == symbol
+    end
+  end
+  total
+end
+
+puts 'Welcome to Tic Tac Toe!'.center(28)
+loop do
+  m = [
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [' ', ' ', ' ']
+  ]
+
+  loop do
+    single_ary_free_spots = free_spots(m).flatten(1).map(&:join)
+
+    if single_ary_free_spots.all?(&:empty?)
+      puts "there is no winner, it's a tie!"
+      break
+    end
+
+    display_matrix(m)
+
+    puts "Please choos one free spot, it will be market with an 'X'"
+    puts "your free spots are:"
+    puts ''
+    display_spots(m)
+    puts ''
+    puts "You are 'X', computer is 'O'"
+    puts ''
+    puts 'choose a row and column/item'
+    puts 'For example top left is 00, bottom right 22, center 11'
+
+
+    row_col_usr = gets.chomp
+
+    until single_ary_free_spots.include?(row_col_usr)
+      puts ''
+      puts 'Error:'
+      puts "#{row_col_usr} not valid, please check again the valid options:"
+      puts ''
+      display_spots(m)
+      puts 'choose a row and column/item'
+      row_col_usr = gets.chomp
+    end
+
+    play(row_col_usr, 'X', m)
+
+    player_results = show_player_results(m, 'X')
+
+    if player_won?(player_results)
+      display_matrix(m)
+      puts 'Player won!'
+      break
+    end
+
+    row_col_computer = single_ary_free_spots.sample
+    play(row_col_computer, 'O', m)
+
+    computer_results = show_player_results(m, 'O')
+
+    if player_won?(computer_results)
+      display_matrix(m)
+      puts 'Computer won!'
+      break
+    end
+  end
+  puts "to you want to play again? 'Yes/y' 'no' or other key to finish game"
+  play_again = gets.chomp
+  unless play_again.downcase == 'y' || play_again.downcase == 'yes'
+    puts "Thank you for playing Tic Tac Toe, hope to see you soon!"
+    break
+  end
+end
+
+
+=begin
+  Problem understanding:
+  - mental model:
+    from tictactoe nested array, return true if the player 'X' or 'O' as won
+    * use one method for both but test separetely
+    * we have the win results collection WIN_RESULTS(above)
+
+  Examples (below)
+
+  D
+  Algorithm:
+    - iterate each row
+    from each row compare each of the win results, see if all are present
+    * maybe with `any?` for true
+  C
+=end
+
+# def player_won?(matrix)
+#   WIN_RESULTS.map {|win_res| win_res.all? { |el| matrix.include? el } }.any?
+# end
+
+# m_no_win_x = ["00", "02", "12", "21"]
+# m_no_win_o = ["01", "10", "11", "20", "22"]
+
+# player_won?(m_no_win_x) == false
+# player_won?(m_no_win_o) == false
+
+# m_x_won = ["00", "11", "22"]
+# player_won?(m_x_won) == true
+
+# m_x_won = ["00", "02", "11", "22"]
+
+# player_won?(m_x_won) == true
+
+# m_o_won = ["00", "01", "02", "22"]
+
+# player_won?(m_o_won)  ==  true
+
+# is_a_winner = ["01", "02", "10", "12", "22"]
+
+# player_won?(is_a_winner)  ==  true
+
+
+# def player_won?(matrix)
+#   WIN_RESULTS.map do |win_res|
+#     p win_res
+#     win_res.all? do |el|
+#       p el
+#       p matrix.include? el
+#     end
+#   end.any?
+# end
