fc6978af73c021cdc94a3144786dd11cf79ca8b5
Tonio Serna <antonelo@gmail.com>
2021-03-17 16:42:31 +0100

    Add live assessment examples folder and & add hashtag generator exercise

---
+++ 101-109/live_assessment_examples/hashtag_generator_CW24_5kyu_medium_easy.rb
@@ -0,0 +1,44 @@
+=begin
+24. The Hashtag Generator
+(https://www.codewars.com/kata/52449b062fb80683ec000024)
+5 kyu
+The marketing team is spending way too much time typing in hashtags.
+Let's help them with our own Hashtag Generator!
+
+Here's the deal:
+
+It must start with a hashtag (#).
+All words must have their first letter capitalized.
+If the final result is longer than 140 chars it must return false.
+If the input or the result is an empty string it must return false.
+Examples
+" Hello there thanks for trying my Kata"  =>  "#HelloThereThanksForTryingMyKata"
+"    Hello     World   "                  =>  "#HelloWorld"
+""                                        =>  false
+=end
+
+def no_spaces_str(str)
+  str.chars.select {|char| char != ' '}.join
+end
+
+
+def generateHashtag(str)
+  strip_string = str.strip
+
+  return false if strip_string.empty? || no_spaces_str(str).size >= 140
+
+  capitalized_ary = strip_string.split.map(&:capitalize)
+
+  capitalized_ary.unshift('#').join
+end
+
+p generateHashtag("") == false
+p generateHashtag(" " * 200) == false
+p generateHashtag("Do We have A Hashtag") == "#DoWeHaveAHashtag"
+p generateHashtag("Codewars") == "#Codewars"
+p generateHashtag("Codewars Is Nice") ==  "#CodewarsIsNice"
+p generateHashtag("Codewars is nice") == "#CodewarsIsNice"
+p generateHashtag("code" + " " * 140 + "wars") == "#CodeWars"
+p generateHashtag("Looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong Cat") == false
+p generateHashtag("a" * 139) == "#A" + "a" * 138
+p generateHashtag("a" * 140) == false
