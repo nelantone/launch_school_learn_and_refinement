e1c13c966b512a29689d41578b77ccf8bde435ee
Tonio Serna <antonelo@gmail.com>
2021-05-01 17:04:21 +0200

    Add live assessment: 'longest palindrome' medium-hard video 5.1 Srdjan-TA

---
+++ 101-109/live_assessment_examples/longest_palindrome_video5_1_srdjan_medium_hard.rb
@@ -0,0 +1,97 @@
+# Find the length of the longest substring in the given string that is the
+# same in reverse.
+
+# As an example, if the input was "I like racecars that go fast", the substring
+# (racecar) length would be 7.
+
+# If the length of the input string is 0, return value must be 0.
+
+# Example:
+# "(a)" -> 1
+# "(a|a)b" -> 2
+# "(a)bcde" -> 1
+# "zz(ba|ab)cd" -> 4
+#"(ba|ab) lkj (1234|5|4321)33d" => When is odd size we need to include the item in the middle as
+# "123454321".size => 9
+# '(rac|e|car)' => "racecar".size => 7
+# "" -> 0
+
+
+
+=begin
+* Problem understanding:
+  - Problem domain:
+    - Palindorme: characters that are the same writen as in reverse. "racecar".reverse== racecar
+
+  - Mental model:
+    - find the longest substring that reverse is the ame substring and return the size/length  
+
+  - Input: string
+
+  - Output: number
+
+  - Rules:
+    - Explicit:
+      - reverse usbstrings needs to be equal to the original
+
+    - Implicit:
+      - all inputs are downcase and letters/numbers, seem to be also spaces possible.
+
+  - Questions:
+
+* Examples:
+#"(ba|ab) lkj (1234|5|4321)33d" => When is odd size we need to include the item in the middle as
+# "123454321".size => 9
+# '(rac|e|car)' => "racecar".size => 7
+# "" -> 0
+* Data Structure:
+    - array of substring candidates
+
+* Algorithm:
+  - Main Algorithm:
+    - create substring of characters form the bigges to the smallest.
+      - if there is no palindorme biger than 2-3 return 1(unless is an empty string (then 0)
+    - from the substring check the reverse version to see if is palindrome 
+      - If it is palindorm add it into a palindromes array add it as substring otherwise continue
+    - find the longes palindorme substring and return the value.
+
+  - Ideas:
+    - create substring of characters form the bigges to the smallest.
+
+  - Higher Level Algorithm(helper methods):
+    - find palindromes
+
+* Code:
+=end
+
+def find_palindromes(str)
+  palindromes = []
+
+  (str.size).times do |size|
+    str.chars.each_cons(size+1) do |candidate_ary|
+      candidate = candidate_ary.join
+      palindromes << candidate if candidate == candidate.reverse
+    end
+  end
+  palindromes
+end
+
+def longest_palindrome(str)
+  return 0 if str.empty?
+  find_palindromes(str).map(&:size).max
+end
+
+# p find_palindromes("a") == ["a"]
+# p find_palindromes("baabcd") == ["b", "baab"]
+# p find_palindromes("baab1232") == ["b", "baab", "2", "232"]
+
+p longest_palindrome("") == 0
+p longest_palindrome("a") == 1
+p longest_palindrome("aa") == 2
+p longest_palindrome("baa") == 2
+p longest_palindrome("aab") == 2
+p longest_palindrome("baabcd") == 4
+p longest_palindrome("baablkj12345432133d") == 9
+p longest_palindrome("I like racecars that go fast") == 7
+
+
