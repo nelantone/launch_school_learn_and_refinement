7bc3ef940555cf8cd06f7fbe7e8c5359c8d37d87
Tonio Serna <antonelo@gmail.com>
2021-05-21 16:24:27 +0200

    Small problems: Add 12.3 ADV transpose 3x3 easy(when not mutating arg)

--- 101-109/exercises-small_problems/12_3_from_adv_but_easy_transpose_3x3.rb
+++ 101-109/exercises-small_problems/12_3_from_adv_but_easy_transpose_3x3.rb
@@ -1,4 +1,71 @@
 # advaned 1- number 3
+# Secon try (by my own)
+=begin
+Problem Understanding:
+- Mental model:
+  transpose an 3X3 matrix
+- input: matrix/array
+- ouput: array
+
+- rules:
+  - explicit:
+    - don't use `transpose` or/and Matrix class
+    - don't modify the original matrix
+
+Example:
+  - below
+Data struct:
+  - array
+Algorithm:
+  - main algorithm:
+    - store all elements at index n in each subarray in a new subarray
+    - return the subaray inside an array
+
+  - methods
+    - each_with_index
+
+  - pseudocode
+    + initialize a result array
+    + init t_subary
+    + init count(idx_count)
+    + iterate the times of the size of the array
+    + from each iteration take the element at idx_count and stor each element in the subary
+    + store the subary in the result
+    + reassign/re-initialize t_subary
+    + add +1 in the count to continue to the next index.
+    + return the result
+=end
+
+#Example
+matrix = [
+  [1, 5, 8],
+  [4, 7, 2],
+  [3, 9, 6]
+]
+
+def transpose(matrix)
+  result    = []
+  t_subary  = []
+  idx_count = 0
+
+  matrix.size.times do
+    matrix.each { |subary| t_subary << subary[idx_count] }
+    result << t_subary
+    t_subary = []
+
+    idx_count +=1
+  end
+  result
+end
+
+
+new_matrix = transpose(matrix)
+
+p new_matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
+p matrix == [[1, 5, 8], [4, 7, 2], [3, 9, 6]]
+
+
+# First try PP with Mia
 =begin
 Write a method that takes a 3 x 3 matrix in Array of Arrays format and returns the transpose of the original matrix. Note that there is a Array#transpose method that does this -- you may not use it for this exercise. You also are not allowed to use the Matrix class from the standard library. Your task is to do this yourself.
 
@@ -30,25 +97,25 @@ Algorithm
 
 =end
 
-def transpose(mtx)
-  transposed_matrix = []
-  mtx[0].each_with_index do |_first_row_elem, idx|
-    sub_arr = []
-    (0..2).each do |num_in_range|
-      sub_arr << mtx[num_in_range][idx]
-    end
-    transposed_matrix << sub_arr
-  end
-  transposed_matrix
-end
+# def transpose(mtx)
+#   transposed_matrix = []
+#   mtx[0].each_with_index do |_first_row_elem, idx|
+#     sub_arr = []
+#     (0..2).each do |num_in_range|
+#       sub_arr << mtx[num_in_range][idx]
+#     end
+#     transposed_matrix << sub_arr
+#   end
+#   transposed_matrix
+# end
 
-matrix = [
-  [1, 5, 8],
-  [4, 7, 2],
-  [3, 9, 6]
-]
+# matrix = [
+#   [1, 5, 8],
+#   [4, 7, 2],
+#   [3, 9, 6]
+# ]
 
-new_matrix = transpose(matrix)
+# new_matrix = transpose(matrix)
 
-p new_matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
-p matrix == [[1, 5, 8], [4, 7, 2], [3, 9, 6]]
+# p new_matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
+# p matrix == [[1, 5, 8], [4, 7, 2], [3, 9, 6]]
