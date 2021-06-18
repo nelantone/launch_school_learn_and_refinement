c58b0973ad01695de3d81f1bb5f229d8d72c8623
Tonio Serna <antonelo@gmail.com>
2021-06-18 17:23:11 +0200

    Small problems: Add 11.5 'Triangle sides'

--- 101-109/exercises-small_problems/11_5_triangle_sides.rb
+++ 101-109/exercises-small_problems/11_5_triangle_sides.rb
@@ -62,6 +62,85 @@ def triangle(side1, side2, side3)
   end
 end
 
+# Triangle Sides
+# A triangle is classified as follows:
+
+# equilateral All 3 sides are of equal length
+# isosceles 2 sides are of equal length, while the 3rd is different
+# scalene All 3 sides are of different length
+# To be a valid triangle, the sum of the lengths of the two shortest sides must be greater than the length of the longest side, and all sides must have lengths greater than 0: if either of these conditions is not satisfied, the triangle is invalid.
+
+# Write a method that takes the lengths of the 3 sides of a triangle as arguments, and returns a symbol :equilateral, :isosceles, :scalene, or :invalid depending on whether the triangle is equilateral, isosceles, scalene, or invalid.
+
+# Examples:
+# LONG
+=begin
+* Problem understanding:
+  - Problem domain:
+
+  - Mental model:
+
+  - Input:
+
+  - Output:
+
+  - Rules:
+    - Explicit:
+
+    - Implicit:
+
+  - Questions:
+
+* Examples:
+
+* Data Structure:
+    -
+
+* Algorithm:
+  - Main Algorithm:
+
+  - Ideas:
+
+  - Apporach:
+
+  - Higher Level Algorithm(helper methods):
+
+* Code:
+//   - Apporach:
+//     + initalize `sides` array adding the 3 values and sort the array in ascending order
+//       + check if the triangle is valid
+//        + Use `some` to find out if we have a zero value then return 'invalid'
+//        + sum of the two shortest index0 + inedx1 values  must be greater than the longest
+//        +  if the sum of first and second elem is smaller than the third return invalid (as we know we only have 3 elements)
+//      +  check if the trianlge is "equilateral"
+//        +find if in the array we have a uniq number(size one)
+//      +  check if the triangle is isosceles
+//         - find if the array we have 2 uniq numbers(size two)
+ //     - else
+//       - scalene
+=end
+
+def triangle(num1,num2,num3)
+  sides = [num1,num2,num3]#.sort
+  
+  if sides.include?(0) || sides.min(2).sum < sides.max
+    :invalid    
+  elsif sides.uniq.size == 1
+    :equilateral
+  elsif sides.uniq.size == 2
+    :isosceles
+  else
+    :scalene
+  end
+end
+
+p triangle(3, 3, 3) == :equilateral
+p triangle(3, 3, 1.5) == :isosceles
+p triangle(3, 4, 5) == :scalene
+p triangle(0, 3, 3) == :invalid
+p triangle(3, 1, 1) == :invalid
+p triangle(3,5,10)  == :invalid    
+
 # Examples:
 
 triangle(3, 3, 3) == :equilateral
