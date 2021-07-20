86356e8df51876118c72d6bd5b3d8ac69153a4e0
Tonio Serna <antonelo@gmail.com>
2021-07-20 09:15:20 +0200

    1.10

---
+++ 120/oop_with_ruby_book_ls/exercises-oo_programming/1_10_walk_the_cat.rb
@@ -0,0 +1,34 @@
+# Walk the Cat
+# Using the following code, create a module named Walkable that contains a
+# method named #walk. This method should print Let's go for a walk! when
+# invoked. Include Walkable in Cat and invoke #walk on kitty.
+
+module Walkable
+  def walk
+    puts "Let's go for a walk"
+  end
+end
+
+class Cat
+  include Walkable # `include` is for instances
+  # `extend` can be using as class method
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def greet
+    puts "Hello! My name is #{name}!"
+  end
+end
+
+kitty = Cat.new('Sophie')
+kitty.greet
+kitty.walk
+
+# Expected output:
+
+# Hello! My name is Sophie!
+# Let's go for a walk!
